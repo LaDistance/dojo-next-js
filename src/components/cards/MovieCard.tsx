683c2cac4a8a872beff797d27ee0750098cb114e
Quentin <synkx@hotmail.fr>
2023-01-20 21:21:39 +0100

    db schemas and MovieCard component

---
+++ src/components/cards/MovieCard.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import { IMovie } from "../../types/movie";
+import styles from "./MovieCard.module.css";
+
+export default function MovieCard(props: { movie: IMovie }) {
+  const { movie } = props;
+
+  const imageUrl = "https://image.tmdb.org/t/p/w500";
+
+  return (
+    <>
+      {movie && (
+        <div className={styles.card}>
+          <div className={styles.cardImageWrapper}>
+            <Image
+              className={styles.cardImage}
+              src={`${imageUrl}${movie.poster_path}`}
+              alt={movie.title}
+              width="250"
+              height="375"
+            ></Image>
+          </div>
+          <p className={styles.cardInfo}>{movie.release_date}</p>
+          <p className={styles.cardTitle}>{movie.title}</p>
+        </div>
+      )}
+    </>
+  );
+}
