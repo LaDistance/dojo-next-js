1c78309e611ffaa8bbfbc70344e6e205967be7c1
Quentin <synkx@hotmail.fr>
2023-01-31 19:05:45 +0100

    fix dom

--- src/components/cards/MovieCardDetail.tsx
+++ src/components/cards/MovieCardDetail.tsx
@@ -45,11 +45,11 @@ export default function MovieCardDetail({ movie }: { movie: Movie }) {
           ))}
         </section>
         <div className={styles.margin}>
-          <p>
+          <div>
             <Rate allowHalf disabled defaultValue={movie.vote_average / 2} />(
             {movie.vote_average})
             <small className={styles.margin}>{movie.vote_count} votes</small>
-          </p>
+          </div>
         </div>
         <section className={styles.description}>{movie.overview}</section>
       </section>
