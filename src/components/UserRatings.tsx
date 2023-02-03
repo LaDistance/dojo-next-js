989165a3ff47b4eaf99d9fc3f4e1b9a41ea3613b
Quentin <synkx@hotmail.fr>
2023-02-03 09:51:10 +0100

    remove

--- src/components/UserRatings.tsx
+++
@@ -1,8 +0,0 @@
-import type { Movie } from "../server/api/routers/movies";
-import styles from "./RateAndComment.module.css";
-
-const RateAndComment = ({ movie }: { movie: Movie }) => {
-  return <div className={styles.userRating}>{movie.rating}</div>;
-};
-
-export default RateAndComment;
