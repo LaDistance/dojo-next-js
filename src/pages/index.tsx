89a5c584cee43f32bfab711b506455c0f6e5ef7c
antoine.laurent <antoine.laurent@reseau.eseo.fr>
2023-03-27 18:11:40 +0200

    refacto index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,16 @@ import { api } from "../utils/api";
 import styles from "./index.module.css";
 
 const Home: NextPage = () => {
-  const movies = api.movies.getPopular.useQuery();
+  const { data, isLoading, isError, error } = api.movies.getPopular.useQuery();
 
-  if (movies.isLoading) return <div>Loading...</div>;
+  if (isLoading) return <div>Loading...</div>;
 
-  if (movies.isError) return <div>Error: {movies.error.message}</div>;
+  if (isError) return <div>Error: {error.message}</div>;
 
   return (
     <>
       <h1 className={styles.title}>Popular movies</h1>
-      <MovieCollection movies={movies.data} />
+      <MovieCollection movies={data} />
     </>
   );
 };
